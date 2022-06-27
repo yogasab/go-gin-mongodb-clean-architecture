61bfeb3842dbb5bb748f03a49b3fe45d9d98dc7c
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-06-27 08:47:15 +0700

    initial commit create folder, file structure, and entity of user domain

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-gin-mongodb-clean-architecture
+
+go 1.17
+
+require go.mongodb.org/mongo-driver v1.9.1
---
+++ user/entity.go
@@ -0,0 +1,20 @@
+package user
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID             primitive.ObjectID `bson:"id"`
+	Name           string             `bson:"name"`
+	Email          string             `bson:"email"`
+	Password       string             `bson:"password"`
+	AvatarFileName string             `bson:"avatar"`
+	Role           string             `bson:"role"`
+	Location       string             `bson:"location"`
+	Occupation     string             `bson:"occupation"`
+	CreatedAt      time.Time          `bson:"created_at"`
+	UpdatedAt      time.Time          `bson:"updated_at"`
+}
