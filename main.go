41f0a15328181b7b57c58f4e6a3078136610c951
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-06-27 10:22:57 +0700

    create and initialize http router with gin and user domain api

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"go-gin-mongodb-clean-architecture/routes"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	routes.InitializeRoutes(router)
+
+	router.Run(":5000")
 }
--- routes/api.go
+++ routes/api.go
@@ -1,1 +1,16 @@
 package routes
+
+import (
+	userRepo "go-gin-mongodb-clean-architecture/app/repositories/user"
+	userServ "go-gin-mongodb-clean-architecture/app/services/user"
+	"go-gin-mongodb-clean-architecture/db"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitializeRoutes(router *gin.Engine) {
+	// user
+	userCollection := db.GetCollection(db.DB, "users")
+	userRepository := userRepo.NewUserRepository(userCollection)
+	_ = userServ.NewService(userRepository)
+}
