4052ada283f73baada33d773d50742e3d1db8ce6
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-07-04 11:08:57 +0700

    create campaign repository to find campaign by slug

--- app/repositories/campaign/campaign.repository.go
+++ app/repositories/campaign/campaign.repository.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"fmt"
 	"go-gin-mongodb-clean-architecture/app/entities"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"time"
 )
 
 type Repository interface {
 	Create(campaign entities.Campaign) (string, error)
+	FindBySlug(slug string) (entities.Campaign, error)
 }
 
 type repository struct {
@@ -31,4 +33,18 @@ func (r *repository) Create(campaign entities.Campaign) (string, error) {
 
 	insertedID := fmt.Sprintf("%s", result.InsertedID)
 	return insertedID, nil
+}
+
+func (r *repository) FindBySlug(slug string) (entities.Campaign, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var campaign entities.Campaign
+	filter := bson.M{"slug": slug}
+	err := r.campaignCollection.FindOne(ctx, filter).Decode(&campaign)
+	if err != nil {
+		return campaign, err
+	}
+
+	return campaign, nil
 }
