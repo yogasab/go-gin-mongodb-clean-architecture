6e328d1b939a67553aa1011608a305f095f27e98
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-07-04 09:03:42 +0700

    create campaign entitiy

---
+++ app/entities/campaign.entity.go
@@ -0,0 +1,21 @@
+package entities
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Campaign struct {
+	ID               primitive.ObjectID `bson:"_id"`
+	User             primitive.ObjectID `bson:"user"`
+	Title            string             `bson:"title"`
+	ShortDescription string             `bson:"short_description"`
+	Description      string             `bson:"description"`
+	Perks            string             `bson:"perks"`
+	BackerCount      int                `bson:"backer_count"`
+	GoalAmount       int                `bson:"goal_amount"`
+	CurrentAmount    int                `bson:"current_amount"`
+	Slug             string             `bson:"slug"`
+	CreatedAt        time.Time          `bson:"created_at"`
+	UpdatedAt        time.Time          `bson:"updated_at"`
+}
