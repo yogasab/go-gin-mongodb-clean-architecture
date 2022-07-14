2c5794dfe904d783765190294aced5715809f978
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-07-14 10:57:20 +0700

    create find transaction by id reposuitory and unit test

--- app/repositories/transaction/test/transaction_test.go
+++ app/repositories/transaction/test/transaction_test.go
@@ -7,6 +7,7 @@ import (
 	transactionRepo "go-gin-mongodb-clean-architecture/app/repositories/transaction"
 
 	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 var (
@@ -21,5 +22,17 @@ func TestFindAllTransaction(t *testing.T) {
 	}
 
 	assert.NotNil(t, transactions)
+	assert.NoError(t, err)
+}
+
+func TestFindByIDTransacton(t *testing.T) {
+	objID, _ := primitive.ObjectIDFromHex("62ce383116ab6dcc787cc583")
+
+	transaction, err := transactionRepository.FindByID(objID)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	assert.NotNil(t, transaction)
 	assert.NoError(t, err)
 }
