637fe95a56d94043445d1fa3beb6187212013633
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-07-15 19:45:33 +0700

    create store transaction repository test

--- app/repositories/transaction/test/transaction_test.go
+++ app/repositories/transaction/test/transaction_test.go
@@ -4,6 +4,7 @@ import (
 	"go-gin-mongodb-clean-architecture/db"
 	"testing"
 
+	"go-gin-mongodb-clean-architecture/app/entities"
 	transactionRepo "go-gin-mongodb-clean-architecture/app/repositories/transaction"
 
 	"github.com/stretchr/testify/assert"
@@ -34,5 +35,19 @@ func TestFindByIDTransacton(t *testing.T) {
 	}
 
 	assert.NotNil(t, transaction)
+	assert.NoError(t, err)
+}
+
+func TestCreateTransaction(t *testing.T) {
+	objID := primitive.NewObjectID()
+	objCampaignID, _ := primitive.ObjectIDFromHex("62c272d6c7cc6524da5a03e2")
+	objUserID, _ := primitive.ObjectIDFromHex("62bd416dd08bdf54fe7ed518")
+
+	newTransaction, err := transactionRepository.Create(entities.Transaction{ID: objID, Campaign: objCampaignID, User: objUserID, Amount: 120000, Status: "PENDING"})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	assert.NotNil(t, newTransaction)
 	assert.NoError(t, err)
 }
