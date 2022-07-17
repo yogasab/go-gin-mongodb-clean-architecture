1464ee7cd2dc923a60627e65a051f87bf86f366c
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-07-17 13:33:39 +0700

    create unit test to check user avability and get user by email service

--- app/services/user/test/user_test.go
+++ app/services/user/test/user_test.go
@@ -1,9 +1,11 @@
 package test
 
 import (
+	"go-gin-mongodb-clean-architecture/app/dto"
 	userRepo "go-gin-mongodb-clean-architecture/app/repositories/user"
 	userServ "go-gin-mongodb-clean-architecture/app/services/user"
 	"go-gin-mongodb-clean-architecture/db"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -34,6 +36,32 @@ func TestGetUserByID(t *testing.T) {
 	}
 
 	assert.NotNil(t, user)
+	assert.Equal(t, "62bbc5f1a7dbcd9b551b7db5", user.ID.Hex())
 	assert.Equal(t, "Rodri", user.Name)
+	assert.NoError(t, err)
+}
+
+func TestGetUserByEmail(t *testing.T) {
+	user, err := userService.GetUserByEmail("rodrygo@gmail.com")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.NotNil(t, user)
+	assert.Equal(t, "rodrygo@gmail.com", user.Email)
+	assert.Equal(t, "string", reflect.TypeOf(user.Email).String())
+	assert.Equal(t, "Rodri", user.Name)
+	assert.NoError(t, err)
+}
+
+func TestCheckUserAvailability(t *testing.T) {
+	isAvailable, err := userService.CheckUserAvailability(dto.CheckUserAvailabilityInput{Email: "rodrygo@gmail.com"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.NotNil(t, isAvailable)
+	assert.Equal(t, false, isAvailable)
+	assert.Equal(t, "bool", reflect.Bool.String())
 	assert.NoError(t, err)
 }
